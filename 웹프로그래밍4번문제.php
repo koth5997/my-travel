3dd512b8f9d1ec6703f32c744d9cd3561adec74d
koth5997 <127705834+koth5997@users.noreply.github.com>
2023-04-10 18:18:27 +0900

    Add files via upload

---
+++ 웹프로그래밍4번문제.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+웹프로그래밍 4번문제<br>
+box area1 =<?php echo $_POST["width"]*$_POST["height"];?><br>
+
+box area2=<?php echo $_POST["width"]*$_POST["height"]/2;?><br>
+
+box area3=<?php echo $_POST["pi"]*($_POST["radius"]**2);?><br>
+
+box area4=<?php echo $_POST["width"]*$_POST["length"]*$_POST["height"];?><br>
+
+box area5=<?php echo $_POST["pi"]*($_POST["radius"]**2)*$_POST["height"];?><br>
+
+box area6=<?php echo $_POST["pi"]*($_POST["radius"]**3)*(4/3);?><br>
+</body>
+</html>
