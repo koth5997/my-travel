38b62a9fe42befe72057489342290042d51a3e86
koth5997 <127705834+koth5997@users.noreply.github.com>
2024-11-10 16:51:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Sidebar 열기
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+// Sidebar 닫기
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
